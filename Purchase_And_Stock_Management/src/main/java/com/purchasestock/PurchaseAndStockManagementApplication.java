ee9e5e0c4705a5eb7c4f3e4a0bb2582da5f5d891
T MUKESH RAJA <tmukeshrajatmr10@gmail.com>
2024-07-22 11:53:36 +0530

    Application Setup
    1) Structure Created
    2) Database Added

---
+++ Purchase_And_Stock_Management/src/main/java/com/purchasestock/PurchaseAndStockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.purchasestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseAndStockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseAndStockManagementApplication.class, args);
+	}
+
+}
---
+++ Purchase_And_Stock_Management/src/test/java/com/purchasestock/PurchaseAndStockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purchasestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseAndStockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
